7c37f279fb4b77ce89ab18b7b942b896d600d2ca
Izza Irena <izzairena03@gmail.com>
2025-10-25 11:17:57 +0800

    Update kelolaTahunAkademik.php

--- SE/dinasPendidikan/kelolaTahunAkademik.php
+++ SE/dinasPendidikan/kelolaTahunAkademik.php
@@ -75,7 +75,6 @@ $tahun_list = $conn->query("SELECT * FROM tahun_akademik ORDER BY id DESC");
       <li><a href="kelolaTahunAkademik.php"><i class="fa-solid fa-calendar-days"></i> Tahun Akademik</a></li>
       <li><a href="aturanSeleksi.php"><i class="fa-solid fa-scale-balanced"></i> Aturan Seleksi</a></li>
       <li><a href="kelolaPendaftaranDinas.php" class="active"><i class="fa-solid fa-users"></i> Kelola Pendaftaran</a></li>
-      <li><a href="monitoring.php"><i class="fa-solid fa-chart-line"></i> Monitoring</a></li>
       <li><a href="logout.php"><i class="fa-solid fa-right-from-bracket"></i> Logout</a></li>
     </ul>
 </aside>
@@ -151,3 +150,4 @@ $tahun_list = $conn->query("SELECT * FROM tahun_akademik ORDER BY id DESC");
 </div>
 </body>
 </html>
+
